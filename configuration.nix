c75d100e8cf6f51c0160c2a2615bffbfcace4a91
pancho horrillo <pancho@pancho.name>
2020-08-04 17:05:15 +0200

    Track latest kernel
    See https://nixos.wiki/wiki/Linux_kernel

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
